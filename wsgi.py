c121d9cb3e319eeff68f0f10c8c9669d00de0a03
Laddge <dev.laddge@gmail.com>
2022-10-18 03:50:03 +0000

    create app

---
+++ wsgi.py
@@ -0,0 +1,24 @@
+import requests
+
+
+def app(environ, start_response):
+    wsgi_input = environ.get("wsgi.input")
+    req_data = None
+    if wsgi_input:
+        req_data = wsgi_input.read()
+    req_headers = {}
+    for k, v in environ.items():
+        if k[:5] == "HTTP_":
+            req_headers[k[5:]] = v
+    req = requests.Request(
+        environ.get("REQUEST_METHOD"),
+        environ.get("REQUEST_URI").split("?", 1)[1],
+        data=req_data,
+        headers=req_headers,
+    )
+    s = requests.Session()
+    res = s.send(req.prepare())
+    data = res.content
+    status = "{} {}".format(res.status_code, res.reason)
+    start_response(status, res.headers)
+    return [data]
