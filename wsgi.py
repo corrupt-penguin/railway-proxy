aec2dc3c05abb6b9fb359c408f06c844cb514efd
Laddge <dev.laddge@gmail.com>
2022-10-18 07:38:19 +0000

    raw ip

--- wsgi.py
+++ wsgi.py
@@ -13,9 +13,15 @@ def app(environ, start_response):
     host = ""
     if "HOST" in req_headers:
         host = req_headers.pop("HOST")
+    url = environ.get("RAW_URI")
+    if url[:3] == "/r/":
+        url = url[3:]
+        req_headers["X_FORWARDED_FOR"] = environ.get("REMOTE_ADDR")
+    else:
+        url = url[1:]
     req = requests.Request(
         environ.get("REQUEST_METHOD"),
-        environ.get("RAW_URI")[1:],
+        url,
         data=req_data,
         headers=req_headers,
     )
@@ -26,20 +32,20 @@ def app(environ, start_response):
         html = res.text
         html = html.replace("href=\"https://", "href=\"" + host_url + "https://")
         html = html.replace("href=\"http://", "href=\"" + host_url + "http://")
-        html = html.replace("href=\"//", "href=\"" + host_url + environ.get("RAW_URI")[1:].split(":")[0] + "://")
-        html = html.replace("href=\"/", "href=\"" + host_url + "/".join(environ.get("RAW_URI")[1:].split("/")[:3]) + "/")
+        html = html.replace("href=\"//", "href=\"" + host_url + url.split(":")[0] + "://")
+        html = html.replace("href=\"/", "href=\"" + host_url + "/".join(url.split("/")[:3]) + "/")
         html = html.replace("href='https://", "href='" + host_url + "https://")
         html = html.replace("href='http://", "href='" + host_url + "http://")
-        html = html.replace("href='//", "href='" + host_url + environ.get("RAW_URI")[1:].split(":")[0] + "://")
-        html = html.replace("href='/", "href='" + host_url + "/".join(environ.get("RAW_URI")[1:].split("/")[:3]) + "/")
+        html = html.replace("href='//", "href='" + host_url + url.split(":")[0] + "://")
+        html = html.replace("href='/", "href='" + host_url + "/".join(url.split("/")[:3]) + "/")
         html = html.replace("src=\"https://", "src=\"" + host_url + "https://")
         html = html.replace("src=\"http://", "src=\"" + host_url + "http://")
-        html = html.replace("src=\"//", "src=\"" + host_url + environ.get("RAW_URI")[1:].split(":")[0] + "://")
-        html = html.replace("src=\"/", "src=\"" + host_url + "/".join(environ.get("RAW_URI")[1:].split("/")[:3]) + "/")
+        html = html.replace("src=\"//", "src=\"" + host_url + url.split(":")[0] + "://")
+        html = html.replace("src=\"/", "src=\"" + host_url + "/".join(url.split("/")[:3]) + "/")
         html = html.replace("src='https://", "src='" + host_url + "https://")
         html = html.replace("src='http://", "src='" + host_url + "http://")
-        html = html.replace("src='//", "src='" + host_url + environ.get("RAW_URI")[1:].split(":")[0] + "://")
-        html = html.replace("src='/", "src='" + host_url + "/".join(environ.get("RAW_URI")[1:].split("/")[:3]) + "/")
+        html = html.replace("src='//", "src='" + host_url + url.split(":")[0] + "://")
+        html = html.replace("src='/", "src='" + host_url + "/".join(url.split("/")[:3]) + "/")
         html = html.replace("hxxp", "http")
         res_data = html.encode()
     else:
